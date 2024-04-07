d2827675972cf53c5e2a5837684dae618969df55
fahlev-dev <rfahlevi501@gmail.com>
2024-04-07 15:35:07 +0700

    belajar casting tipe data

---
+++ python_dasar_07.py
@@ -0,0 +1,17 @@
+# mengambil input dari user
+
+# data yang di masukan bertipe string
+# data = input("Masukan data : ")
+
+# print("data = ", data, "type = ", type(data))
+
+# angka = int(input("Masukan angka : "))
+# print("data = ", angka, "type = ", type(angka))
+
+# angka = float(input("Masukan angka : "))
+# print("data = ", angka, "type = ", type(angka))
+
+biner = bool(int(input("Masukan nilai boolean : ")))
+
+print("data = ", biner, "type = ", type(biner))
+
