a3974264290abd76f7b56b39facb1fcc9155ed4d
fahlev-dev <rfahlevi501@gmail.com>
2024-04-07 16:19:00 +0700

    belajar operasi aritmatika

---
+++ python_dasar_08.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+
+# operasi tambah
+hasil = a + b
+print(a, '+', b, '=', hasil)
+
+# operasi pengurangan
+hasil = a - b
+print(a, '-', b, '=', hasil)
+
+# operasi perkalian
+hasil = a * b
+print(a, '*', b, '=', hasil)
+
+# operasi pembagian
+hasil = a / b
+print(a, '/', b, '=', hasil)
+
+# operasi pangkat
+hasil = a ** b
+print(a, '**', b, '=', hasil)
+
+# operasi modulus
+hasil = a % b
+print(a, '%', b, '=', hasil)
+
+# operasi tambah
+hasil = a // b
+print(a, '//', b, '=', hasil)
