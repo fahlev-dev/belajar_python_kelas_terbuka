f6b0172ea6d4fa2831c196e9142ce0ca3a5ae64d
fahlev-dev <rfahlevi501@gmail.com>
2024-04-09 19:20:56 +0700

    belajar konversi suhu python

---
+++ python_dasar_09.py
@@ -0,0 +1,42 @@
+# latihan konversi satuan temperatur
+
+# celcius
+
+# celcius = float(input("masukan suhu dalam celcius : "))
+
+# reamur = (5 / 4) * celcius
+
+# print("suhu dalam reamur : ", reamur)
+
+# fahrenheit = (9 / 5) * celcius + 32
+
+# print("suhu dalam fahrenheit : ", fahrenheit)
+
+# kelvin = celcius + 273
+
+# print("suhu dalam kelvin : ", kelvin)
+
+# Reamur
+
+# reamur = float(input("Masukan Suhu : "))
+
+# celcius = (4 / 5) * reamur
+# print("suhu dalam celcius : ", celcius)
+
+# fahrenheit = (9 / 4) * reamur + 32
+# print("suhu dalam fahrenheit : ", fahrenheit)
+
+# kelvin = (5 / 4) * reamur + 273
+# print("suhu dalam kelvin : ", kelvin)
+
+# fahrenheit
+# fahrenheit = float(input("Masukan suhu : "))
+
+# celcius = (5 / 9) * (fahrenheit - 32)
+# print("suhu dalam celcius : ", celcius)
+
+# reamur = (4 / 9) * (fahrenheit - 32)
+# print("suhu dalam reamur : ", reamur)
+
+# kelvin = 5 / 9 * (fahrenheit - 32) + 273
+# print("suhu dalam kelvin : ", kelvin)
