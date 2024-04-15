6b320fae026e3aa7b02a3534baf6821505ee2251
fahlev-dev <rfahlevi501@gmail.com>
2024-04-15 20:29:10 +0700

    belajar operator assignment

---
+++ python_dasar_14.py
@@ -0,0 +1,34 @@
+a = 5
+print("nilai a :", a)
+
+a += 1
+print("nilai a += 1, nilai a menjadi : ", a)
+
+a -= 2
+print("nilai a -= 2, nilai a menjadi : ", a)
+
+a *= 5
+print("nilai a *= 5, nilai a menjadi : ", a)
+
+a /= 2
+print("nilai a /= 1, nilai a menjadi : ", a)
+
+b = 10
+print("nilai b =", b)
+
+b %= 3
+print("nilai b %= 3, nilai b menjadi : ", b)
+
+b = 10
+print("nilai b =", b)
+
+b //= 3
+print("nilai b //= 3, nilai b menjadi : ", b)
+
+a = 5
+print("nilai a :", a)
+a **= 3
+print("nilai a **= 3, nilai a menjadi : ", a)
+
+
+
