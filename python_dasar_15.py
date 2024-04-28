c7441d451c4dae86157e48b4bca6f751249f3fca
fahlev-dev <rfahlevi501@gmail.com>
2024-04-28 15:53:29 +0700

    belajar string python

---
+++ python_dasar_15.py
@@ -0,0 +1,37 @@
+# pengenalan string
+
+data = "ini adalah string"
+print(data)
+print(type(data))
+
+print('"halo apa kabar"')
+print("'halo apa kabar'")
+print("ini adalah hari jum'at")
+
+print('mari shalat jum\'at')
+print('g\'day, isn\'t it?')
+
+print("C:\\user\\ucup")
+
+print("ucup\t\t\totong, semakin jauhan")
+
+print("ucup \botong, jadi deketan")
+
+print("baris pertama. \nbaris kedua.")
+print("baris pertama.\rbaris kedua.")
+print("baris pertama.\r\nbaris kedua.")
+
+print('C:\new folder')
+
+print(r'C:\new folder')
+
+print("""
+nama : ucup
+kelas : 3 sd
+ """)
+
+print(r""" 
+Nama : ucup
+kelas : 3\sd
+website : www.ucup.com/newID
+""")
