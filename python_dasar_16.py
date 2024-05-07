369a2bc6c4c2fd7f9d52ef4925c88efca22a813f
fahlev-dev <rfahlevi501@gmail.com>
2024-05-07 22:16:35 +0700

    belajar string

---
+++ python_dasar_16.py
@@ -0,0 +1,14 @@
+nama_pertama = "ucup"
+nama_tengah = "D"
+nama_akhir = "fame"
+
+nama_lengkap = nama_pertama + " " + nama_tengah + " " + nama_akhir
+
+print(nama_lengkap)
+
+panjang = len(nama_lengkap)
+print("panjang dari " + nama_lengkap + " = " + str(panjang))
+
+d = "ucup"
+status = d in nama_lengkap
+print(d + " ada di " + nama_lengkap + " = " + str(status))
