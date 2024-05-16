e55b6e7b74db283c872a1c00225db54b0fecc65a
fahlev-dev <rfahlevi501@gmail.com>
2024-05-16 13:30:31 +0700

    string with alignment

---
+++ python_dasar_19.py
@@ -0,0 +1,37 @@
+data_nama = "ucup sarucup"
+data_umur = 17
+data_tinggi = 150.1
+data_nomor_sepatu = 44
+
+# string standar
+data_string = f"nama = {data_nama}, umur = {data_umur}, tinggi = {data_tinggi}, sepatu = {data_nomor_sepatu}"
+print(5*"="+"data string"+5*"=")
+print(data_string)
+
+# string multiline 
+data_string = f"nama = {data_nama}, \numur = {data_umur}, \ntinggi = {data_tinggi}, \nsepatu = {data_nomor_sepatu}"
+print("\n"+5*"="+"data string"+5*"=")
+print(data_string)
+
+# string multiline (kutip triplets)
+data_string = """
+nama = {data_nama}
+umur = {data_umur}
+tinggi = {data_tinggi}
+sepatu = {data_nomor_sepatu}
+"""
+
+print("\n"+5*"="+"data string"+5*"=")
+print(data_string)
+
+# mengatur lebar
+data_nama = "Ucup"
+data_string = f"""
+nama = {data_nama:>5}
+umur = {data_umur:>5}
+tinggi = {data_tinggi:>5}
+sepatu = {data_nomor_sepatu:>5}
+"""
+
+print("\n"+5*"="+"data string"+5*"=")
+print(data_string)
