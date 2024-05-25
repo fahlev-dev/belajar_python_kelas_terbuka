7ba0a7e686d106cc7a65ff63eb5349ba8a281bf9
fahlev-dev <rfahlevi501@gmail.com>
2024-05-25 11:50:48 +0700

    belajar if dan else statment

---
+++ python_dasar_21.py
@@ -0,0 +1,19 @@
+# if dan else statement
+
+umur = int(input("Masukan umur : "))
+
+if umur > 17:
+    print("Anda boleh mencoblos")
+else:
+    print("Anda belum bisa mencoblos")
+
+print("terima kasih")
+
+nama = input("masukan nama: ")
+
+if nama == "otong":
+    print("selamat datang otong")
+else:
+    print("kamu bukan otong ya")
+
+print("ini adalah akhir program")
