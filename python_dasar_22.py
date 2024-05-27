d7307f0754cbb8622d9bcd1d5957247aba0829ec
fahlev-dev <rfahlevi501@gmail.com>
2024-05-27 22:33:04 +0700

    belajar elif sederhana

---
+++ python_dasar_22.py
@@ -0,0 +1,10 @@
+# ELIF else if statement
+
+nama = input("nama anda siapa: ")
+
+if nama == "ucup":
+    print("ini adalah ucup")
+elif nama == "otong":
+    print("ini adalah otong")
+
+print("program selesai")
