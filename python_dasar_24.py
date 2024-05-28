c81775787eb84b87ad8c9820460af43337bc72e2
fahlev-dev <rfahlevi501@gmail.com>
2024-05-28 09:02:06 +0700

    belajar for loop

---
+++ python_dasar_24.py
@@ -0,0 +1,22 @@
+# for loop
+
+# ini dengan list
+angka_list = [0,1,2,3,4,5,6,7,8,9,10]
+
+for i in angka_list:
+    print(f"i sekarang -> {i}")
+
+print("Akhir dari program 1")
+
+# ini dengan range
+angka_range = range(5)
+
+for i in angka_range:
+    print(f"i sekarang -> {i}")
+
+print("akhir dari program 2")
+
+data_str = "ini adalah sebuah kalimat"
+
+for huruf in data_str:
+    print(huruf)
