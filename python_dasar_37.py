9dbf96aac2303649eac84178d02497c748a086f7
fahlev-dev <rfahlevi501@gmail.com>
2024-06-12 19:52:44 +0700

    belajar tuples dan set

---
+++ python_dasar_37.py
@@ -0,0 +1,20 @@
+# list
+data_list = [1,2,3,4,5,6,7,8,9,10]
+print(data_list)
+
+# tuples
+data_tuples = (1,2,3,4,5,6,7,8,9,10)
+print(data_tuples)
+print(data_tuples[1])
+
+# data tuple tidak bisa di lakukan assignment
+# data_tuples[1] = 10
+# data_tuples.append(11)
+
+# set
+data_sets = {1,2,3,4,5,6,7,8,9,10}
+print(data_sets)
+
+# print(data_sets[2]) karena urutan datanya tidak berurutan, atau tidak punya index
+
+
