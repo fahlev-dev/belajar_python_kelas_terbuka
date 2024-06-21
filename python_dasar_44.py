97d2a3b960ec9d7fda40bf138efb9c46bb8034a6
fahlev-dev <rfahlevi501@gmail.com>
2024-06-21 15:52:48 +0700

    belajar fungsi dasar

---
+++ python_dasar_44.py
@@ -0,0 +1,11 @@
+# belajar membuat fungsi di python
+
+def hello_world():
+    """
+    fungsi hello world
+    """
+    print("hello world!!!")
+    print("kepada ucup surucup")
+    print("kepada otong surotong")
+
+hello_world()
