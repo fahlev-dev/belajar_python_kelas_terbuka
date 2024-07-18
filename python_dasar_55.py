1959df2ccde2549aa16752a858958eacf745dced
fahlev-dev <rfahlevi501@gmail.com>
2024-07-18 07:17:16 +0700

    belajar membuat modul di python

---
+++ python_dasar_55.py
@@ -0,0 +1,12 @@
+# module matematika dengan import 
+
+import matematika as m 
+
+hasil_tambah = m.tambah(1,2,3,4,5)
+print(f"hasil tambah = {hasil_tambah}")
+
+hasil_kali = m.kali(1,2,3,4,5)
+print(f"hasil kali = {hasil_kali}")
+
+pangkat_3 = m.pangkat(3)
+print(f"hasil pangkat = {pangkat_3(3)}")
