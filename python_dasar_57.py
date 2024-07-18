9c8ff39de4966a762ad50d926ab1322ccf3245d5
fahlev-dev <rfahlevi501@gmail.com>
2024-07-18 15:56:18 +0700

    belajar init di python

---
+++ python_dasar_57.py
@@ -0,0 +1,10 @@
+import sains.matematika
+import sains.fisika
+
+hasil_tambah = sains.matematika.tambah(1,2,3,4,5)
+print(f"hasil tambah adalah = {hasil_tambah}")
+
+hasil_fisika = sains.fisika.gaya(90, 10)
+print(f"gaya adalah = {hasil_fisika}")
+
+import datetime
