d9e9f5af6213276b0b8c437547cbe16a7a88a708
fahlev-dev <rfahlevi501@gmail.com>
2024-07-18 07:37:19 +0700

    belajar package python

---
+++ python_dasar_56.py
@@ -0,0 +1,13 @@
+import sains.matematika
+from sains import fisika
+from sains.fisika import gaya as g
+
+hasil_tambah = sains.matematika.tambah(1,2,3,4,5)
+print(f"hasil tambah dari package adalah = {hasil_tambah}")
+
+gaya = g(90, 10)
+print(f"gaya adalah = {gaya}")
+
+
+
+
